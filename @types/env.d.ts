552638fcc4c16ef0bfff9cf8e53b00fe2f60b743
Radosław Miluk <VibingCreator@gmail.com>
2023-04-14 13:06:20 +0200

    update environment variables types

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -3,11 +3,8 @@ declare global {
     interface ProcessEnv {
       DISCORD_SECRET: string;
       DISCORD_CLIENT_ID: string;
-      DISCORD_GUILD_ID: string;
       GITEA_SECRET: string;
       GITEA_BASE_URL: string;
-      GITEA_USERNAME: string;
-      GITEA_REPOSITORY: string;
       OPENAI_SECRET: string;
     }
   }
